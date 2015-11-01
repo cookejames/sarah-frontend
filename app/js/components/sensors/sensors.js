fd74d668e5ee618181844e0aeb6e5364316b5fbb
James Cooke <james@cookeit.co.uk>
2015-11-01 19:43:58 +0000

    Remove old sensor pages

--- app/js/components/sensors/sensors.js
+++
@@ -1,8 +0,0 @@
-class SensorsController {
-    /*@ngInject*/
-    constructor() {
-    }
-}
-angular.module('sarahApp.sensors', []);
-register('sarahApp.sensors').
-    controller('SensorsController', SensorsController);
